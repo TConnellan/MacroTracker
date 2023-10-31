2cf5475b109a36c983fc4e8f12d7a6c6276dc59f
TConnellan <tconnellan@Xubuntu.myguest.virtualbox.org>
2023-10-31 14:58:32 +1100

    refactoring to mvc

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const consumedRouter = require("./controllers/consumed")
+const config = require('./utilities/config')
+const logger = require('./utilities/logger')
+const middleware = require('./utilities/middleware')
+
+morgan.token('postData', function (req, res) {return JSON.stringify(req.body)})
+
+const app = express()
+app.use(cors()) // enable...
+app.use(express.json())
+app.use(express.static('build'))
+
+//maybe eliminate one of the following
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
+app.use(middleware.requestLogger)
+
+app.use('/api/consumed', consumedRouter)
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ backend/controllers/consumed.js
@@ -0,0 +1,100 @@
+const consumedRouter = require("express").Router()
+const { pool } = require("./dbPool")
+const logger = require("../utilities/logger")
+
+
+
+consumedRouter.get("/:userid&:date", (request, response) => {
+    const userID = Number(request.params.userid)
+    logger.logInfo(`id was${userID}`);
+    const date = request.params.date
+    logger.logInfo(`date was: ${date}`);
+    const query = 
+`
+SELECT carbs, fats, proteins 
+FROM consumed
+WHERE user_id = 1 AND consumed_at IS NULL;
+`   
+    pool.connect() // will be based on parameters in environment
+        .then((client) => {
+            logger.logInfo("Made Connection")
+            return client.query(query)
+                        .then(resp => {
+                            response.json(resp.rows)
+                            logger.logInfo("Query Successful")
+                        })
+                        .catch(err => logger.logError(`Error in query: ${err}`))
+                        .finally(client => {
+                            client.release()
+                            logger.logInfo("Client Released")
+                        })
+        })
+        .catch(err => {
+            logger.logError(`error connecting to db: ${err}`)
+        })
+        
+
+        
+        
+})
+
+
+consumedRouter.post("/addconsumable", (request, response) => {
+    const data = request.body
+    logger.logInfo(request);
+    const values = [data.name,data.brand_name,data.size,data.units,data.carbs,data.fats,data.proteins]
+    const query=
+`
+INSERT into consumable(cons_name, brand_name, size, units, carbs, fats, proteins) values ($1,$2,$3,$4,$5,$6,$7);
+`
+    pool.connect()
+        .then(client => {
+            logger.logInfo("Made Connection")
+            return client.query(query, values)
+                        .then(resp => {
+                            response.status(201)
+                            response.send(resp)
+                            logger.logInfo("Query Successful")
+                        })
+                        .catch(err => logger.logError(`Error in query: ${err}`))
+                        .finally((client) => {
+                            client.release()
+                            logger.logInfo("Client Released")
+                        })
+        })
+        .catch(err => {
+            logger.logError(`error connecting to db: ${err}`)
+        })
+})
+
+consumedRouter.post("/addconsumed", (request, response) => {
+    const data = request.body
+    logger.logInfo(request.body);
+    const values = [data.user_id,data.recipe_id,data.quantity,data.carbs,data.fats,data.proteins,data.consumed_at,data.created_at,data.last_edited_at,data.notes]
+    const query=
+`
+INSERT into consumed(user_id, recipe_id, quantity, carbs, fats, proteins, consumed_at, created_at, last_edited_at, notes) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);
+`
+    pool.connect()
+        .then(client => {
+            logger.logInfo("Made Connection")
+            client.query(query, values)
+                .then(resp => {
+                    response.status(201)
+                    response.send(resp)
+                    logger.logInfo("Query Successful")
+                })
+                .catch(err => logger.logError(`Error in query: ${err}`))
+                .finally((client) => {
+                    client.release()
+                    logger.logInfo("Client Released")
+                })
+        })
+        .catch(error => {
+            logger.logError(`error connecting to db: ${err}`)
+            response.status(500)
+            response.send()
+        })
+})
+
+module.exports = consumedRouter
